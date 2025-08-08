87bee0d55a2e8751ae2a1bec03fb452d97175732
Yash Mehta <yashsinghmehta37@gmail.com>
2025-08-09 02:44:46 +0530

    tweaks

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-app_name="pedia"
+app_name="encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
